054461fd3f3189b665a93e655bbc3bf1421be2db
AndrArajo <andre.arajo07@gmail.com>
2022-08-31 20:56:21 -0300

    Alteração teste e mensagem de olá mundo

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Olá Mundo :)"
+    return "Olá Mundo Docker :)"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
